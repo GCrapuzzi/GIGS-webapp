1fdf85d32438068112f31d98222f48f7c43e64c9
GionniGi <gcrapuzzi02@gmail.com>
2024-08-13 17:26:32 +0200

    chore: Update backend and frontend package.json files

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,7 +1,23 @@
-// server.js
-const app = require('./app');
-const PORT = process.env.PORT || 5000;
+// app.js
+const express = require('express');
+const mongoose = require('mongoose');
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+const app = express();
+
+// Middleware (questa andrebbe in middleware, fallo quando vuoi paul)
+app.use(express.json()); // Per il parsing del corpo delle richieste in formato JSON
+
+// Connessione al database (questa andrebbe spostata in controller, fallo quando vuoi paul)
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log('MongoDB connected'))
+.catch(err => console.log('MongoDB connection error:', err));
+
+// Routes (questa potrebbe andare in routes ma essendo la route principale la lascerei qua, a te la scelta)
+app.get('/', (req, res) => {
+  res.send('API is running...');
 });
+
+module.exports = app;
---
+++ backend/src/server.js
@@ -0,0 +1,11 @@
+// server.js
+const app = require('./app');
+const http = require('http');
+
+const PORT = process.env.PORT;
+
+const server = http.createServer(app);
+
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
