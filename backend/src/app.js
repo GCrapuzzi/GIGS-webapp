c884a0515668841cfe9a1c4d6aaa0d12dbf953d5
GionniGi <gcrapuzzi02@gmail.com>
2024-08-13 17:46:08 +0200

    chore: Update backend package.json and add CORS dependency

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,11 +1,13 @@
 // app.js
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const app = express();
 
 // Middleware (questa andrebbe in middleware, fallo quando vuoi paul)
 app.use(express.json()); // Per il parsing del corpo delle richieste in formato JSON
+app.use(cors()); // Per consentire le richieste da qualsiasi origine
 
 // Connessione al database (questa andrebbe spostata in controller, fallo quando vuoi paul)
 mongoose.connect(process.env.MONGO_URI, {
