6b3003fcc5027468f6e9d242f6293640a2e7f5a6
GionniGi <gcrapuzzi02@gmail.com>
2024-08-20 10:22:18 +0200

    chore: Update dependencies and refactor controllers

--- backend/src/controllers/verifyNumber.js
+++ backend/src/controllers/verifyNumber.js
@@ -4,13 +4,29 @@ import { generateOTP } from "../utils/generateOTP";
 
 const verifyNumber = async (req, res) => {
     const { number } = req.body;
+
+    // Verifica se il numero è valido
     if (!validateNumber(number)) {
         return res.status(400).json({ message: "Numero non valido" });
     }
-    const numberExist = await User.findOne({ number });
-    if (numberExist){
-        
+
+    // Cerca l'utente con il numero specificato
+    let user = await User.findOne({ number });
+
+    // Genera un OTP
+    const otp = generateOTP();
+
+    if (user) {
+        // Se l'utente esiste, aggiorna il campo otp
+        user.otp = otp;
+        await user.save();
+    } else {
+        // Se l'utente non esiste, crea un nuovo utente
+        user = new User({ number, otp });
+        await user.save();
     }
+
+    return res.status(200).json({ message: "OTP generato con successo", otp });
 }
 
 module.exports = verifyNumber;
