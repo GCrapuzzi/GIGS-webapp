4fe8a8090970f492383a5888a1c26fa6f577dfb1
GionniGi <gcrapuzzi02@gmail.com>
2024-08-25 17:36:18 +0200

    chore: Update Otp.jsx to remove unused code and fetch data from backend

--- frontend/src/pages/Otp.jsx
+++ frontend/src/pages/Otp.jsx
@@ -2,14 +2,6 @@ import Footer from "../components/Footer.jsx";
 import HomepageContainer from "../components/HomepageContainer.jsx";
 
 function Otp({buttonText, backgroundImage, toggleButtonState, buttonState}){
-    
-    // Fetch dei dati da backend
-    const [data, setData] = useState([]);
-    useEffect(() => {
-         fetch("http://localhost:5000/api/otp")
-             .then((res) => res.json())
-             .then((data) => setData(data));
-    }, []);
 
     return(
         <>
