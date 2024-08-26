a6707a99bda6683ddb78fc01b1cbee243a3ff291
GionniGi <gcrapuzzi02@gmail.com>
2024-08-26 18:46:18 +0200

    chore: Add error handling to verifyToken controller

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -6,9 +6,11 @@ const auth = require("../middleware/authMiddleware");
 const verifyUser = require("../controllers/verifyUser");
 const authenticate = require("../controllers/authenticate");
 const verifyToken = require("../controllers/verifyToken");
+const logout = require("../controllers/logout");
 
 router.post("/verify", verifyUser);
 router.post("/authenticate", authenticate);
 router.get("/loggedin", auth, verifyToken);
+router.get("/logout", auth, logout);
 
 module.exports = router;
