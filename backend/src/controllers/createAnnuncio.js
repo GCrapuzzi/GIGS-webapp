8ccfd275d944f939f28fea67cac05acdddfd8144
GionniGi <gcrapuzzi02@gmail.com>
2024-08-26 19:04:41 +0200

    chore: Update createAnnuncio controller to use user_id field instead of number

--- backend/src/controllers/createAnnuncio.js
+++ backend/src/controllers/createAnnuncio.js
@@ -3,27 +3,18 @@ const Annunci = require('../models/annuncioSchema');
 
 // Funzione per creare un annuncio con la foto profilo dell'utente
 async function createAnnuncio(req, res, next) {
-    const { number,città, lavoretto, titolo, descrizione, token, tariffa, orario } = req.body;
+    const { città, lavoretto, titolo, descrizione, userId, tariffa, orario } = req.body;
 
     try {
-        // Da cambiare -> utilizzare userId estratto tramite middleware dal token
-        const utenteCreatore = await User.findOne({ number: number });
-
-        if (!utenteCreatore) {
-            return res.status(404).json({ error: 'Utente non trovato per il numero di telefono fornito' });
-        }
-
-        // Genera dinamicamente il link al profilo dell'utente
-        const profiloLink = `${req.protocol}://${req.get('host')}/users/${userId}`;
-
         // Crea un nuovo annuncio con il link al profilo dell'utente
         const nuovoAnnuncio = new Annunci({
             titolo: titolo,
             descrizione: descrizione,
             città: città,
             lavoretto: lavoretto,
-            userId: utenteCreatore._id, // Riferimento all'utente che ha creato l'annuncio
-            link: profiloLink, // Link al profilo dell'utente creatore
+            user_id: userId,
+            tariffa: tariffa,
+            orario: orario
         });
 
         // Salva il nuovo annuncio nel database
