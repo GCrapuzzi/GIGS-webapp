f06d2addaa4ae8179b3e975d7292aa9cc379e5e1
paul0202 <stegapaolo@gmail.com>
2024-08-26 18:56:58 +0200

    annunci

--- backend/src/controllers/createAnnuncio.js
+++ backend/src/controllers/createAnnuncio.js
@@ -3,25 +3,20 @@ const Annunci = require('../models/annuncioSchema');
 
 // Funzione per creare un annuncio con la foto profilo dell'utente
 async function createAnnuncio(req, res, next) {
-    const { number,città, lavoretto, titolo, descrizione, token, tariffa, orario } = req.body;
+    const { number, città, lavoretto, titolo, descrizione, token,tariffa, orario, userId } = req.body;
 
     try {
-        // Da cambiare -> utilizzare userId estratto tramite middleware dal token
-        const utenteCreatore = await User.findOne({ number: number });
-
-        if (!utenteCreatore) {
-            return res.status(404).json({ error: 'Utente non trovato per il numero di telefono fornito' });
-        }
 
         // Genera dinamicamente il link al profilo dell'utente
         const profiloLink = `${req.protocol}://${req.get('host')}/users/${utenteCreatore._id}`;
 
         // Crea un nuovo annuncio con il link al profilo dell'utente
-        const nuovoAnnuncio = new Ad({
+        const nuovoAnnuncio = new Annunci({
             titolo: titolo,
             descrizione: descrizione,
             città: città,
             lavoretto: lavoretto,
+            tariffa: tariffa,
             userId: utenteCreatore._id, // Riferimento all'utente che ha creato l'annuncio
             link: profiloLink, // Link al profilo dell'utente creatore
             fotoProfilo: utenteCreatore.profileImageUrl // Foto profilo dell'utente creatore
