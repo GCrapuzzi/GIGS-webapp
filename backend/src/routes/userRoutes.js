13058ce9114fb32183b3e8661b99ca91a3dec28f
paul0202 <stegapaolo@gmail.com>
2024-09-04 21:53:02 +0200

    completeAccount

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -7,10 +7,12 @@ const verifyUser = require("../controllers/signupUser");
 const authenticate = require("../controllers/authUser");
 const verifyToken = require("../controllers/validateToken");
 const logout = require("../controllers/logoutUser");
+const completeAccount = require("../controllers/completeAccount");
 
 router.post("/verify", verifyUser);
 router.post("/authenticate", authenticate);
 router.get("/loggedin", auth, verifyToken);
 router.get("/logout", auth, logout);
+router.post("/completeAccount", auth, completeAccount);
 
 module.exports = router;
