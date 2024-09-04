add1d5ac9ac1be2cc501eed751d320d02df37c29
cristiansaracino <info.cristiansaracino@gmail.com>
2024-09-04 19:48:22 +0200

    annunci listing

--- frontend/src/components/SearchForm.jsx
+++ frontend/src/components/SearchForm.jsx
@@ -1,16 +1,44 @@
+import axios from "axios"
+import { useState } from "react";
 import { GiGardeningShears, GiPositionMarker } from "react-icons/gi"
 
+
+
 function SearchForm({buttonVisitorStyle, buttonText}){
+
+    const [citta, setCitta] = useState('');
+    const [tipoLavoro, setTipoLavoro] = useState('');
+
+    const handleSearch = async (event) => {
+        event.preventDefault();
+        const data = {
+            citta: citta,
+            tipoLavoro: tipoLavoro
+        };
+
+        try{
+            const response = await axios.get('http://localhost:5000/annunci/listing', {
+                params: data
+            });
+            if(response.status === 200){
+                console.log(response.annuncio)
+            }
+        }catch (error) {
+            console.error('Error submitting form:', error.response ? error.response.data : error.message);
+        }
+
+    }
+
     return(
-        <form action="" className="HomepageForm">
+        <form onSubmit={handleSearch} className="HomepageForm">
             <div className="textContainer">
                 <div>
                     <GiPositionMarker className="icon" />
-                    <input type="text" placeholder="Inserisci città:" className="formSpace"/>
+                    <input type="text" placeholder="Inserisci città:" className="formSpace" value={citta} onChange={(e) => setCitta(e.target.value)} />
                 </div>
                 <div>
                     <GiGardeningShears className="icon" />
-                    <input type="text" placeholder="Inserisci Lavoretto da cercare:" list="jobs" className="formSpace"/>
+                    <input type="text" placeholder="Inserisci Lavoretto da cercare:" list="jobs" className="formSpace" value={tipoLavoro} onChange={(e) => setTipoLavoro(e.target.value)}/>
                     <datalist id="jobs">
                         <option value="Fotografo" />
                         <option value="Sguattera" />
@@ -20,7 +48,7 @@ function SearchForm({buttonVisitorStyle, buttonText}){
                     </datalist>
                 </div>
             </div>
-            <button action="submit" className="submitButton" style={buttonVisitorStyle}>{buttonText}</button>
+            <button type="submit" className="submitButton" style={buttonVisitorStyle}>{buttonText}</button>
         </form>
     )
 }
