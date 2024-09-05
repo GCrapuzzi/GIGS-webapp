eedb644601c9daa7080c13e8554ed062a540ea33
GionniGi <gcrapuzzi02@gmail.com>
2024-09-05 19:03:31 +0200

    refactor: Update annuncioSchema to use mongoose reference for userId

--- backend/src/models/annuncioSchema.js
+++ backend/src/models/annuncioSchema.js
@@ -8,7 +8,7 @@ const annuncioSchema=mongoose.Schema({
       tariffa: String, 
       descrizione: String,
       orario: String,
-      userId: ObjectId
+      userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }
 })
 
 // Modello per gli annunci
