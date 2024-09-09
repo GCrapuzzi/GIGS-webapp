e2371c6b8c4f99702b1553da7a4be3bc02912bfd
cristiansaracino <info.cristiansaracino@gmail.com>
2024-09-09 19:55:41 +0200

    filtraggio

--- frontend/src/pages/Cardpage.jsx
+++ frontend/src/pages/Cardpage.jsx
@@ -1,16 +1,81 @@
-import React from 'react';
-import { useLocation } from 'react-router-dom';  // Importa useLocation
+import React, { useState } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';  // Importa useLocation
 import Card from "../components/Card";
 import LoginPage from "../components/LoginPage";
+import { GiGardeningShears } from 'react-icons/gi';
+import SearchCityInput from '../components/SearchCityInput';
+import { VscSettings } from "react-icons/vsc";
+import axios from 'axios';
 
-function Cardpage({ buttonState, toggleButtonState }) {
+function Cardpage({ buttonState, toggleButtonState}) {
   const location = useLocation();  // Usa useLocation per ottenere lo stato
+  const navigate = useNavigate();
   const  { annunci } = location.state || { annunci: [] }; 
+  const [buttonStatus, setButtonStatus] = useState(false);
+  const [formData, setFormData] = useState({
+    città: '',
+    lavoro: '',
+    prezzoMin: '',
+    prezzoMax: '',
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+        ...formData,
+        [name]: value
+    });
+  };
+
+  const setCitta = (e) => {
+    setFormData({
+        ...formData,
+        città: e
+        
+    });
+  }
+
+  const handleFilterForm = async (event) =>{
+    event.preventDefault();
+    try{
+      const response = await axios.get('http://localhost:5000/annunci/filtra', {
+          params: formData
+      });
+      if(response.status === 200){
+          navigate('/cardPage', { state: { annunci: response.data } });
+          console.log(response.data)
+      }
+    }catch (error) {
+        console.error('Error submitting form:', error.response ? error.response.data : error.message);
+    }
+  }
+
   
+
   return (
     <>
-      <div className={`overlay ${buttonState ? 'active' : ''}`}></div>
+      <button onClick={() => setButtonStatus(!buttonStatus)} id="buttonFilter"><VscSettings height = '3x' />Filtri</button>
+      <div className={`filterContainer ${buttonStatus ? 'active' : ''}`}>
+        <form className='filterSubContainer' onSubmit={handleFilterForm}>
+          <h1>Inserisci i parametri per il filtraggio</h1>
+          <input type="text" name="prezzoMin" value={formData.prezzoMin} placeholder='prezzo Min' className='formSpace' onChange={handleChange}/>
+          <input type="text" name="prezzoMax" value={formData.prezzoMax} placeholder='prezzo Max' className='formSpace' onChange={handleChange}/>
+          <GiGardeningShears className="icon" />
+          <input type="text" placeholder="Inserisci Lavoretto da offrire:" list="jobs" value={formData.lavoro} className="formSpace" name="lavoro" onChange={handleChange} />
+            <datalist id="jobs">
+              <option value="Fotografo" />
+              <option value="Sguattera" />
+              <option value="Taglia erba" />
+              <option value="Baby-sitter" />
+              <option value="Pet-sitter" />
+            </datalist>
+            <SearchCityInput setCitta={setCitta}/>
+            <button action="submit" onClick={() => setButtonStatus(!buttonStatus)} className="submitButton" id="filterFormButton">Esegui filtraggio</button>
+        </form>
 
+      </div>
+      <div className={`overlay ${buttonState ? 'active' : ''}`}></div>
+      <div className={`overlay2 ${buttonStatus ? 'active' : ''}`}></div>
       <div className="cardContainer">
         {annunci.map((annuncio) => (
           <Card key={annuncio._id} annuncio={annuncio} />
