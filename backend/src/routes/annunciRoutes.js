e2dd9ea3345ee75f8fd1a3c7d5a917d6f567a4ea
paul0202 <stegapaolo@gmail.com>
2024-09-11 15:12:30 +0200

    listing

--- backend/src/routes/annunciRoutes.js
+++ backend/src/routes/annunciRoutes.js
@@ -6,11 +6,13 @@ const auth = require("../middleware/authMiddleware");
 const createAnnuncio = require("../controllers/createAnnuncio");
 const filtraAnnunci = require("../controllers/filtraAnnunci");
 const listingAnnunci = require("../controllers/listingAnnunci");
+const listingAnnunciDiUnUtente = require("../controllers/listingAnnunciDiUnUtente");
 
 
 router.post("/createAnnuncio", auth, createAnnuncio);
 router.get("/filtra", filtraAnnunci);
 router.get("/listing", listingAnnunci);
+router.get("/listingUtente", auth, listingAnnunciDiUnUtente);
 
 
 module.exports = router;
