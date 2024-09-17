1c5bdd73c35ea08decda9026ad3fd42132fc5ea5
cristiansaracino <info.cristiansaracino@gmail.com>
2024-09-17 02:00:00 +0200

    fix change number

--- frontend/src/components/ChangeProfile.jsx
+++ frontend/src/components/ChangeProfile.jsx
@@ -1,105 +1,208 @@
-import axios from "axios"
-import { useEffect, useState } from "react"
+import axios from "axios";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 
-
-function ChangeProfile({utente, setButtonStatus, buttonStatus, notifySuccess, notifyError, showProfile}){
+function ChangeProfile({ utente, setButtonStatus, buttonStatus, notifySuccess, notifyError, showProfile }) {
     const navigate = useNavigate();
+    const [selectedFile, setSelectedFile] = useState(null);
     const [userData, setUserData] = useState({
-        nome: null,
-        cognome: null,
-        biografia: null,
-        oldPhoneNumber: null,
-        newPhoneNumber: null,
-        newPhoneNumberConferm: null,
-    })
+        nome: "",
+        cognome: "",
+        biografia: "",
+    });
 
+    const [phoneData, setPhoneData] = useState({
+        oldPhoneNumber: "",
+        newPhoneNumber: "",
+        newPhoneNumberConferm: "",
+    });
 
     const handleChange = (e) => {
         const { name, value } = e.target;
         setUserData({
             ...userData,
-            [name]: value
+            [name]: value,
         });
     };
 
+    const handlePhoneChange = (e) => {
+        const { name, value } = e.target;
+        setPhoneData({
+            ...phoneData,
+            [name]: value,
+        });
+    };
 
     const handleDelete = async () => {
         try {
-            const response =  await axios.post('http://localhost:5000/users/deleteUserData', {}, {withCredentials:true})
-            navigate("/")
-            notifySuccess("L'account è stato correttamente eliminato")
+            await axios.post("http://localhost:5000/users/deleteUserData", {}, { withCredentials: true });
+            navigate("/");
+            notifySuccess("L'account è stato correttamente eliminato");
         } catch (error) {
-            console.log('fallito')
-            notifyError("L'account non è stato correttamente eliminato")
-            
+            notifyError("L'account non è stato correttamente eliminato");
         }
-    }
+    };
 
-    const onChange = async (event) => {
-        event.preventDefault();
+    const onChangeGeneralData = async (formData) => {
         try {
-
-            const response =  await axios.post('http://localhost:5000/users/updateAccount', userData, {withCredentials:true})
-            notifySuccess("L'account è stato correttamente aggiornato")
-            showProfile()
+            await axios.post("http://localhost:5000/users/updateAccount", formData, { withCredentials: true });
+            notifySuccess("Le generalità sono state correttamente aggiornate");
+            showProfile();
         } catch (error) {
-            
-            notifyError("L'account non è stato correttamente aggiornato")
-            
+            switch (error.response.data.message) {
+                case "Non c'è nulla da modificare":
+                    toast.info("Non c'è nulla da modificare");
+                    break;
+                default:
+                    toast.error('Si è verificato un errore sconosciuto.');
+                    break;
+            }
         }
-        
-    }
+    };
 
-    const handleCloseForm = () => {
-        setButtonStatus(!buttonStatus)
-        
-    }
+    const onChangePhoneData = async (event) => {
+        event.preventDefault();
+        if (phoneData.newPhoneNumber !== phoneData.newPhoneNumberConferm) {
+            notifyError("Il nuovo numero di telefono e la conferma non coincidono");
+            return;
+        }
 
-    return(
+        try {
+            await axios.post("http://localhost:5000/users/updateAccount", phoneData, { withCredentials: true });
+            notifySuccess("Il numero di telefono è stato correttamente aggiornato");
+        } catch (error) {
+            switch (error.response.data.message) {
+                case "Il numero di telefono attuale non corrisponde":
+                    toast.error("Il numero di telefono attuale non corrisponde");
+                    break;
+                default:
+                    toast.error('Si è verificato un errore sconosciuto.');
+                    break;
+            }
+        }
+    };
+
+    const handleFileChange = (e) => {
+        setSelectedFile(e.target.files[0]);
+    };
+
+    const handleSubmitGeneralData = (e) => {
+        e.preventDefault();
+        const formDataToSend = new FormData();
+
+        // Aggiungi il file solo se è stato selezionato
+        if (selectedFile) {
+            formDataToSend.append("fotoProfilo", selectedFile);
+        }
+
+        // Aggiungi solo le generalità
+        formDataToSend.append("nome", userData.nome);
+        formDataToSend.append("cognome", userData.cognome);
+        formDataToSend.append("biografia", userData.biografia);
+
+        // Resetta i campi delle generalità
+        setUserData({
+            nome: "",
+            cognome: "",
+            biografia: "",
+        });
+
+        // Invia il form con le generalità
+        onChangeGeneralData(formDataToSend);
+    };
+
+    return (
         <div className="containerPage">
             <div className="flexContainer">
                 <div className="containerProfile">
-                    <button onClick={handleCloseForm}>X</button>
+                    <button onClick={() => setButtonStatus(!buttonStatus)}>X</button>
                     <h1>Aggiorna il tuo profilo</h1>
+
+                    {/* Form per generalità e foto profilo */}
                     <h2>Aggiorna le tue generalità:</h2>
-                    <form onSubmit={onChange}>
+                    <form onSubmit={handleSubmitGeneralData}>
                         <div>
                             <h3>Aggiorna la tua foto profilo:</h3>
-                            <input type="file" id="fileInput"/>
+                            <input type="file" id="fileInput" onChange={handleFileChange} />
                         </div>
                         <div>
                             <h3>Inserisci il nuovo nome:</h3>
-                            <input type="text" className="formSpace formDetails" name="nome" value={userData.nome} onChange={handleChange}/>
+                            <input
+                                type="text"
+                                className="formSpace formDetails"
+                                name="nome"
+                                value={userData.nome}
+                                onChange={handleChange}
+                            />
                         </div>
                         <div>
                             <h3>Inserisci il nuovo cognome:</h3>
-                            <input type="text" className="formSpace formDetails" name="cognome" value={userData.cognome} onChange={handleChange}/>
+                            <input
+                                type="text"
+                                className="formSpace formDetails"
+                                name="cognome"
+                                value={userData.cognome}
+                                onChange={handleChange}
+                            />
                         </div>
-                        <div>   
-                          <h3>Inserisci la nuova biografia:</h3>                     
-                          <input type="textarea" className="formSpace formDetails" name="biografia" id="description" value={userData.biografia} onChange={handleChange}/>
+                        <div>
+                            <h3>Inserisci la nuova biografia:</h3>
+                            <textarea
+                                className="formSpace formDetails"
+                                name="biografia"
+                                value={userData.biografia}
+                                onChange={handleChange}
+                            />
                         </div>
-                        <button className="submitButton formDetails" type="submit">Invia</button>
+                        <button className="submitButton formDetails" type="submit">
+                            Invia
+                        </button>
                     </form>
 
-
                     <h2>Aggiorna il tuo numero di telefono:</h2>
-                    <form onSubmit={onChange}>
-                    
-                        <input type="text" placeholder="Inserisci il vecchio numero di telefono:" name="oldPhoneNumber" value={userData.oldPhoneNumber} onChange={handleChange} className="formSpace formDetails"/>
-                        <input type="text" placeholder="Inserisci il nuovo numero di telefono:" name="newPhoneNumber" value={userData.newPhoneNumber} onChange={handleChange} className="formSpace formDetails" />
-                        <input type="text" placeholder="Conferma il nuovo numero di telefono" name="newPhoneNumberConferm" value={userData.newPhoneNumberConferm} onChange={handleChange} className="formSpace formDetails"/>
-                        <button className="submitButton formDetails" type="submit">Invia</button>
-                    </form> 
+                    <form onSubmit={onChangePhoneData}>
+                        <input
+                            type="tel"
+                            placeholder="Inserisci il vecchio numero di telefono:"
+                            name="oldPhoneNumber"
+                            value={phoneData.oldPhoneNumber}
+                            onChange={handlePhoneChange}
+                            required pattern='[0-9]{9,10}' title='Il numero di telefono deve avere 9 o 10 cifre numeriche!'
+                            className="formSpace formDetails"
+                        />
+                        <input
+                            type="tel"
+                            placeholder="Inserisci il nuovo numero di telefono:"
+                            name="newPhoneNumber"
+                            value={phoneData.newPhoneNumber}
+                            required pattern='[0-9]{9,10}' title='Il numero di telefono deve avere 9 o 10 cifre numeriche!'
+                            onChange={handlePhoneChange}
+                            className="formSpace formDetails"
+                        />
+                        <input
+                            type="tel"
+                            placeholder="Conferma il nuovo numero di telefono"
+                            name="newPhoneNumberConferm"
+                            required pattern='[0-9]{9,10}' title='Il numero di telefono deve avere 9 o 10 cifre numeriche!'
+                            value={phoneData.newPhoneNumberConferm}
+                            onChange={handlePhoneChange}
+                            className="formSpace formDetails"
+                        />
+                        <button className="submitButton formDetails" type="submit">
+                            Invia
+                        </button>
+                    </form>
 
+                    {/* Pulsante per eliminare profilo */}
                     <h2>Cancella tutti i tuoi dati:</h2>
-                    <button id="deleteButton" onClick={handleDelete}>Elimina il tuo profilo</button>
-                                 
-
+                    <button id="deleteButton" onClick={handleDelete}>
+                        Elimina il tuo profilo
+                    </button>
                 </div>
-                
             </div>
         </div>
-    )
-} export default ChangeProfile
+    );
+}
+
+export default ChangeProfile;
