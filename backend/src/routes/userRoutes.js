300b544a5dd128ac589f040551d09a1e1e5b2929
GionniGi <gcrapuzzi02@gmail.com>
2024-10-29 12:59:30 +0100

    Refactor validateToken.js: Add token verification function

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -7,13 +7,14 @@ const upload = require("../middleware/fotoMiddleware");
 const verifyUser = require("../controllers/signupUser");
 const authenticate = require("../controllers/authUser");
 const logout = require("../controllers/logoutUser");
+const validateToken = require("../controllers/validateToken");
 const updateAccount = require("../controllers/updateAccount");
 const trovaUser = require("../controllers/trovaUser");
 const deleteUserData = require("../controllers/deleteUserdata");
 
 router.post("/verify", verifyUser);
 router.post("/authenticate", authenticate);
-router.get("/loggedin", auth);
+router.get("/loggedin", auth, validateToken);
 router.get("/logout", auth, logout);
 router.get("/trovaUser", auth, trovaUser)
 router.post("/updateAccount", auth, upload.single('fotoProfilo'), updateAccount);
