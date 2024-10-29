fa29a718004c58e63b3563846d56802ced9cb0af
GionniGi <gcrapuzzi02@gmail.com>
2024-10-29 11:09:57 +0100

    Refactor userRoutes.js: Add token validation to /loggedin route

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -6,7 +6,7 @@ const upload = require("../middleware/fotoMiddleware");
 // Importa i controller
 const verifyUser = require("../controllers/signupUser");
 const authenticate = require("../controllers/authUser");
-//const verifyToken = require("../controllers/validateToken");
+const verifyToken = require("../controllers/validateToken");
 const logout = require("../controllers/logoutUser");
 const updateAccount = require("../controllers/updateAccount");
 const trovaUser = require("../controllers/trovaUser");
@@ -14,7 +14,7 @@ const deleteUserData = require("../controllers/deleteUserdata");
 
 router.post("/verify", verifyUser);
 router.post("/authenticate", authenticate);
-router.get("/loggedin", auth);
+router.get("/loggedin", auth, verifyToken);
 router.get("/logout", auth, logout);
 router.get("/trovaUser", auth, trovaUser)
 router.post("/updateAccount", auth, upload.single('fotoProfilo'), updateAccount);
