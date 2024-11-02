65281c0e94ae58a669d7c5210f3f52f736017007
GionniGi <gcrapuzzi02@gmail.com>
2024-11-02 10:55:29 +0100

    Update CORS configuration to include credentials and adjust allowed headers

--- backend/app.js
+++ backend/app.js
@@ -15,8 +15,9 @@ const app = express();
 app.use(express.json());
 app.use(cors({
   origin: 'https://gigs-webapp-frontend.vercel.app',
+  credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
+  allowedHeaders: ['Content-Type', 'Authorization']
 }));
 app.use(cookieParser());
 
